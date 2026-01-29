268f20e24888fa849c8e03c939fb0402c806f583
Maykon Lincoln <maykonlincolnmanager@gmail.com>
2026-01-29 11:59:42 -0300

    Create stream_job.py

---
+++ stream_job.py
@@ -0,0 +1,9 @@
+# Esqueleto para processamento de stream
+
+def process(record):
+    # normalização / enriquecimento
+    return record
+
+if __name__ == '__main__':
+    # conectar ao kafka, consumir, processar e produzir
+    pass
